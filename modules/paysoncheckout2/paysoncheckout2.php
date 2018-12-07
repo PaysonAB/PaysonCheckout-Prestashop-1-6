7c59e933fd168942f47b6cdada4232f7aac2996b
mickeb <mikael.blom@payson.se>
2018-12-07 09:43:41 +0100

    2.0.14 - corrected version number

--- modules/paysoncheckout2/paysoncheckout2.php
+++ modules/paysoncheckout2/paysoncheckout2.php
@@ -29,7 +29,7 @@ class PaysonCheckout2 extends PaymentModule
     {
         $this->name = 'paysoncheckout2';
         $this->tab = 'payments_gateways';
-        $this->version = '2.0.13';
+        $this->version = '2.0.14';
         $this->ps_versions_compliancy = array('min' => '1.6.0.14', 'max' => _PS_VERSION_);
         $this->author = 'Payson AB';
         $this->module_key = '4015ee54469de01eaa9150b76054547e';
